dfa624abc612ecfdb0c8d598451a13d9d717aa8e
Cristian Steve <ccristiansteve@gmail.com>
2020-04-14 13:17:48 -0500

    Definiendo rutas del servidor

---
+++ src/controllers/index.controller.js
@@ -0,0 +1,11 @@
+const indexCtrll = {};
+
+indexCtrll.renderIndex = (req, res) =>{
+    res.render('index');
+};
+
+indexCtrll.renderAbout = (req, res) =>{
+    res.render('about');
+};
+
+module.exports = indexCtrll;
--- src/server.js
+++ src/server.js
@@ -23,9 +23,7 @@ app.use(express.urlencoded({extended: false}));            //Leer formatos tipo 
 //Global variables
 
 //Routes
-app.get('/', (req, res) =>{
-    res.render('index');
-});
+app.use(require('./routes/index.routes'));
 
 //static files
 app.use(express.static(path.join(__dirname, 'public')));   //Definir la ruta publica del proyecto
